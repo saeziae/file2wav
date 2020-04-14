30bd33e8a4297758418a9ebbb760adae95754f41
山前 <saeziae@users.noreply.github.com>
2020-04-14 10:17:40 +0800

    improvement

--- decrypt.py
+++ decrypt.py
@@ -1,28 +1,41 @@
-import pyaudio
+#import pyaudio
 import wave
 from numpy import sin
 import struct
 import bz2
+from tqdm import trange
+import sys
 
-RATE = 48000
-FILE_OUTPUT_FILENAME = "out.pdf"
-WAVE_INPUT_FILENAME = "out.wav"
+
+if len(sys.argv) == 3:
+    WAVE_INPUT_FILENAME = sys.argv[1]
+    FILE_OUTPUT_FILENAME = sys.argv[2]
+elif len(sys.argv) == 2:
+    WAVE_INPUT_FILENAME = sys.argv[1]
+    FILE_OUTPUT_FILENAME = sys.argv[1]+'.bin'
+else:
+    print("format: <in file> [<out file>]")
+    sys.exit()
+
 
 wf = wave.open(WAVE_INPUT_FILENAME, 'rb')
 
-p = pyaudio.PyAudio()
+#p = pyaudio.PyAudio()
 
-stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                channels=wf.getnchannels(),
-                rate=wf.getframerate(),
-                output=True)
+#stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+#                channels=wf.getnchannels(),
+#                rate=wf.getframerate(),
+#                output=True)
 
 data0 = []
-data = wf.readframes(1)
+frames = wf.getnframes()
+#data = wf.readframes(1)
 i = 0
-a = rate / 440
-while data:
-    stream.write(data)
+a = wf.getframerate() / 440
+
+for _ in trange(frames):
+    data = wf.readframes(1)
+    #stream.write(data)
     f = struct.unpack("h", data)[0]
     b = i / a
     c = b * 3.14159 * 2
@@ -31,16 +44,14 @@ while data:
     S = (f - e)//16 + 128
     data0.append(S)
     i += 1
-    data = wf.readframes(1)
-    if i % RATE == 0:
-        print(i)
+    
 
-stream.stop_stream()
-stream.close()
-p.terminate()
+#stream.stop_stream()
+#stream.close()
+#p.terminate()
 print("read")
 
-data0 = b''.join([bytes.fromhex("%02X"%i) for i in data0])
+data0 = b''.join([bytes.fromhex("%02X" % i) for i in data0])
 print("Decompress")
 data0 = bz2.decompress(data0)
 with open(FILE_OUTPUT_FILENAME, "wb") as f:
--- encrypt.py
+++ encrypt.py
@@ -3,34 +3,41 @@ import wave
 from numpy import sin
 import struct
 import bz2
+from tqdm import tqdm
+import sys
+
+
+if len(sys.argv) == 3:
+    IN_FILENAME = sys.argv[1]
+    WAVE_OUTPUT_FILENAME = sys.argv[2]
+elif len(sys.argv) == 2:
+    IN_FILENAME = sys.argv[1]
+    WAVE_OUTPUT_FILENAME = sys.argv[1]+'.wav'
+else:
+    print("format: <in file> [<out file>]")
+    sys.exit()
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 48000
-IN_FILENAME = "in.pdf"
-WAVE_OUTPUT_FILENAME = "out.wav"
 
 data_byte = b''
 with open(IN_FILENAME, "rb") as f:
     print("COMPRESSING")
     data_byte = bz2.compress(f.read(), compresslevel=9)
-w = len(data_byte)
+
 data = []
 i = 0
 a = RATE / 440
-for S in data_byte:
+for S in tqdm(data_byte):
     b = i / a
     c = b * 3.14159 * 2
     d = sin(c)*28670
     e = int(d)
     f = e + (S - 128)*16
     data.append(f)
-
     i += 1
-    if w >= 50:
-        if i % (w//50) == 0:
-            print(str(i*100//w)+"%")
 
 print("WRITE")
 
